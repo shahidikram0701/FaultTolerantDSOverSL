3ec7e95e1ae06372f429c9bc23c05d8f94cb4a4b
saketjajoo <saketjajoo77@gmail.com>
2023-11-10 14:45:08 -0600

    Updating the init file

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
-sudo apt install snapd
-sudo snap install go --classic
+# sudo apt install snapd
+# sudo snap install go --classic
 
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+# echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+# echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 
-go install github.com/mattn/goreman@latest
+# go install github.com/mattn/goreman@latest
+
+
+wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz
+sudo tar -C /usr/local/bin/ -xzf go1.21.3.linux-arm64.tar.gz
+echo 'export PATH=$PATH:/usr/local/bin/go/bin' >> ~/.bashrc
 
 source ~/.bashrc
+
+go mod init github.com/scalog/scalog && go mod tidy && go mod vendor
+
+sed -i 's/1\.21\.1/1\.33\.0/g' go.mod
+
+go mod tidy && go mod vendor && go build .
