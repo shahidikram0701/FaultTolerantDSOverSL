32392ee9b06cc4976d8865f43258119902326edd
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-11-10 17:25:50 -0600

    Deleted vendor files and added cloudlab setup script

---
+++ init-cloudlab.sh
@@ -0,0 +1,11 @@
+vms=("ms0308.utah.cloudlab.us" "ms0313.utah.cloudlab.us" "ms0341.utah.cloudlab.us" "ms0610.utah.cloudlab.us" "ms0315.utah.cloudlab.us" "ms0301.utah.cloudlab.us" "ms0322.utah.cloudlab.us" "ms0607.utah.cloudlab.us" "ms0620.utah.cloudlab.us" "ms0635.utah.cloudlab.us" "ms0330.utah.cloudlab.us" "ms0321.utah.cloudlab.us")
+
+ssh_user="ritwikd2"
+
+for vm in "${vms[@]}"; do
+    echo "Connecting to $vm..."
+    echo "Setting up go and other dependecies  $vm"
+    ssh -o "StrictHostKeyChecking no" "$ssh_user@$vm" "git clone https://github.com/shahidikram0701/FaultTolerantDSOverSL.git && cd ~/FaultTolerantDSOverSL && git checkout feature-ds-scalog && chmod 777 init.sh && ./init.sh"
+
+    echo "-----------------------------------------------------------------------------------"
+done
