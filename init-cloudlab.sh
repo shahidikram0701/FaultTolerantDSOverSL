28d3de7e4fca4c18e448c2aff1e33c475b8fa5b9
saketjajoo <saketjajoo77@gmail.com>
2023-11-17 20:28:29 -0600

    Updating the init script

--- init-cloudlab.sh
+++ init-cloudlab.sh
@@ -5,7 +5,7 @@ ssh_user="ritwikd2"
 for vm in "${vms[@]}"; do
     echo "Connecting to $vm..."
     echo "Setting up go and other dependecies  $vm"
-    ssh -o "StrictHostKeyChecking no" "$ssh_user@$vm" "git clone https://github.com/shahidikram0701/FaultTolerantDSOverSL.git && cd ~/FaultTolerantDSOverSL && git checkout feature-ds-scalog && chmod 777 init.sh && ./init.sh"
+    ssh -o "StrictHostKeyChecking no" "$ssh_user@$vm" "rm -rf FaultTolerantDSOverSL && git clone https://github.com/shahidikram0701/FaultTolerantDSOverSL.git && cd ~/FaultTolerantDSOverSL && git checkout feature-ds-scalog && chmod 777 init.sh && ./init.sh"
 
     echo "-----------------------------------------------------------------------------------"
 done
