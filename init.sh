f3f66f946677728faa561851609773ea73dd61bb
saketjajoo <saketjajoo77@gmail.com>
2023-11-17 22:21:19 -0600

    Download go only when it is not present on the machine

--- init.sh
+++ init.sh
@@ -6,14 +6,17 @@
 
 # go install github.com/mattn/goreman@latest
 
+which go
+if [ "$?" != "0" ]
+then
+    wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz
+    sudo tar -C /usr/local/bin/ -xzf go1.21.3.linux-arm64.tar.gz
+    echo 'export PATH=$PATH:/usr/local/bin/go/bin' >> ~/.bashrc
+    source ~/.bashrc
+    rm -f go1.21.3.linux-arm64.tar.gz
 
-# wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz
-# sudo tar -C /usr/local/bin/ -xzf go1.21.3.linux-arm64.tar.gz
-# echo 'export PATH=$PATH:/usr/local/bin/go/bin' >> ~/.bashrc
-# source ~/.bashrc
-# rm -f go1.21.3.linux-arm64.tar.gz
-
-# echo "Setup go alias and go in PATH"
+    echo "Setup go alias and go in PATH"
+fi
 
 /usr/local/bin/go/bin/go mod init github.com/scalog/scalog && /usr/local/bin/go/bin/go mod tidy && /usr/local/bin/go/bin/go mod vendor || exit 1
 echo "Setup vendor files"
