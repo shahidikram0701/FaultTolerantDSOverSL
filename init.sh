8ffe754fffd0d7cd30e36a1b4d85da6fec27d788
saketjajoo <saketjajoo77@gmail.com>
2023-11-19 11:43:07 -0600

    Adding a flag to kill the running component and then restart it

--- init.sh
+++ init.sh
@@ -29,5 +29,18 @@ echo "Updated gRPC version in go.mod"
 /usr/local/bin/go/bin/go mod tidy && /usr/local/bin/go/bin/go mod vendor && /usr/local/bin/go/bin/go build .
 echo "Syncing libraries and building pkg"
 
-echo "Starting the component based on the .scalog.yaml config file and the machine IP"
-python3 init-cloudlab.py
+if [ "${DEPLOY}" == "1" ]
+then
+    if [ -f *_process_id.log ]
+    then
+        PID=$(cat *_process_id.log)
+        kill -9 $PID || true
+        rm -rf *_process_id.log
+    fi
+
+    PID=$(ps aux | grep "./scalog.*--config" | head -1 | awk '{print $2}')
+    kill -9 $PID || true
+    
+    echo "Starting the component based on the .scalog.yaml config file and the machine IP"
+    python3 init-cloudlab.py
+fi
