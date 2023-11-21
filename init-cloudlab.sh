b8b27c8050f30c76f0d2e78daa24538a2264c429
saketjajoo <saketjajoo77@gmail.com>
2023-11-20 18:42:37 -0600

    Minor changes to deploy script

--- init-cloudlab.sh
+++ init-cloudlab.sh
@@ -7,7 +7,7 @@ ssh_user="ritwikd2"
 for vm in "${vms[@]}"; do
     echo "Connecting to $vm..."
     echo "Setting up go and other dependecies  $vm"
-    ssh -o "StrictHostKeyChecking no" "$ssh_user@$vm" "set DEPLOY=${DEPLOY}; rm -rf FaultTolerantDSOverSL && git clone https://github.com/shahidikram0701/FaultTolerantDSOverSL.git && cd ~/FaultTolerantDSOverSL && git checkout feature-ds-scalog && chmod 777 init.sh && ./init.sh"
+    ssh -o "StrictHostKeyChecking no" "$ssh_user@$vm" "rm -rf FaultTolerantDSOverSL && git clone https://github.com/shahidikram0701/FaultTolerantDSOverSL.git && cd ~/FaultTolerantDSOverSL && git checkout feature-ds-scalog && chmod 777 init.sh && ./init.sh"
 
     echo "-----------------------------------------------------------------------------------"
 done
